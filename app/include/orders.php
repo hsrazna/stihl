56a3813f43c107a478627f823c6d7ff328965936
hsrazna <anzar.terek@gmail.com>
2017-05-19 21:28:22 +0300

    table scroll

--- app/include/orders.php
+++ app/include/orders.php
@@ -4,7 +4,134 @@
 		<a href="#" class="ah-topbox__spanright">Все заказы</a>
 		<p class="az-topbox__p">Введите ваш логин и пароль для того, чтобы зайти на сайт</p>
 	</div>
-	<div class="az-orders__content az-table">
+	<div class="az-orders__content">
+		<div class="az-table">
+			<div class="az-table__top">
+				<table class="az-table__head">
+					<tr class="az-table__row">
+						<td class="az-table__head-td az-table__head-td_center az-table__col1">Дата</td>
+						<td class="az-table__head-td az-table__col2">Название</td>
+						<td class="az-table__head-td az-table__col3">№</td>
+						<td class="az-table__head-td az-table__col4">Кол-во</td>
+						<td class="az-table__head-td az-table__col5">Цена</td>
+						<td class="az-table__head-td az-table__col6">Скидка</td>
+						<td class="az-table__head-td az-table__col7">Итоговая цена</td>
+					</tr>
+				</table>
+			</div>
+			<div class="az-table__bottom">
+				<table class="az-table__body">
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">STIHL Chainsaw Razor 3</td>
+						<td class="az-table__td az-table__col3">1130 200 0356</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">4560.50</td>
+						<td class="az-table__td az-table__col6">15 %</td>
+						<td class="az-table__td az-table__col7">4280.50</td>
+					</tr>
+					<tr class="az-table__row">
+						<td class="az-table__td az-table__td_center az-table__col1">19.03.17</td>
+						<td class="az-table__td az-table__col2">Super Viking Chainsaw</td>
+						<td class="az-table__td az-table__col3">0000 881 0107</td>
+						<td class="az-table__td az-table__col4"><input type="text" class="az-table__input" value="1"></td>
+						<td class="az-table__td az-table__col5">2170.00</td>
+						<td class="az-table__td az-table__col6">2 %</td>
+						<td class="az-table__td az-table__col7">2156.00</td>
+					</tr>
+				</table>
+			</div>
+		</div>
 		<!-- http://jsfiddle.net/avrahamcool/eZXtq/ -->
 	</div>
 </div>
--- app/js/common2.js
+++ app/js/common2.js
@@ -1,7 +1,18 @@
 $(function() {
 	$(".az-menu__icon, .az-menu__link_close").click(function(){
 		$(".az-menu__list").toggleClass("az-menu__list_on");
+		return false;
 	});
 
-
+	setTableBody();
+    $(window).resize(setTableBody);
+    $(".az-table__bottom").scroll(function ()
+    {
+        $(".az-table__top").css({ left: ((-1*parseInt(this.scrollLeft)))+"px" });
+    });
 });
+
+function setTableBody()
+{
+    $(".az-table__bottom").height($(".az-table").height());
+}
