d70cea4ba3afb70a8c02a75a3a53c99b99db5710
hsrazna <anzar.terek@gmail.com>
2017-05-19 16:31:14 +0300

    header mobile and tablet

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -3,5 +3,9 @@ $(function() {
 });
 
 $(function() {
-	// alert(1);
+	$(".az-menu__icon, .az-menu__link_close").click(function(){
+		$(".az-menu__list").toggleClass("az-menu__list_on");
+	});
+
+
 });
