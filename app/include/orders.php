0ea12601be949565300f19a1881f64a91c1703b4
hsrazna <anzar.terek@gmail.com>
2017-05-19 18:35:19 +0300

    sdf

--- app/include/orders.php
+++ app/include/orders.php
@@ -4,5 +4,7 @@
 		<a href="#" class="ah-topbox__spanright">Все заказы</a>
 		<p class="az-topbox__p">Введите ваш логин и пароль для того, чтобы зайти на сайт</p>
 	</div>
-	<div class="az-orders__content"></div>
+	<div class="az-orders__content az-table">
+		<!-- http://jsfiddle.net/avrahamcool/eZXtq/ -->
+	</div>
 </div>
