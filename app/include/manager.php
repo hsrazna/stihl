0a6cf013f9a4c9291c727b0d72d3baa6f47cb0a1
iwebnal <iwebnal@gmail.com>
2017-05-20 18:35:35 +0300

    a

--- app/include/manager.php
+++ app/include/manager.php
@@ -13,35 +13,37 @@
 			<span class="ah-managerinfo-span1">Email:<span>irinam@stihl.com</span></span>
 		</div>
 	</div>
-	<div class="ah-manager__bottombox ah-managerentry">
-		<div class="ah-managerentry__titlebox">
-			<span class="ah-managerentry-title">Сегодня</span>
-		</div>
-		<div class="ah-managerentry__graybox ah-managergray">
-			<div class="ah-managergray__box">
-				<span class="ah-managerentry-span">Здравствуйте!</span>
-				<span class="ah-managerentry-span">Чем могу вам помочь?</span>
+	<div class="az-scroll ah-scrollheight">
+		<div class="ah-manager__bottombox ah-managerentry">
+			<div class="ah-managerentry__titlebox">
+				<span class="ah-managerentry-title">Сегодня</span>
 			</div>
-			<span class="ah-managergray__span">09:30</span>
-		</div>
-		<div class="ah-managerentry__whitebox ah-managerwhite">
-			<div class="ah-managerwhite__box1">
-				<span class="ah-managerwhite__span1">Денег в хату, здоровья семье и близким</span>
+			<div class="ah-managerentry__graybox ah-managergray">
+				<div class="ah-managergray__box">
+					<span class="ah-managerentry-span">Здравствуйте!</span>
+					<span class="ah-managerentry-span">Чем могу вам помочь?</span>
+				</div>
+				<span class="ah-managergray__span">09:30</span>
 			</div>
-			<span class="ah-managerwhite__span">10:15</span>
-		</div>
-		<div class="ah-managerentry__whitebox ah-managerwhite">
-			<div class="ah-managerwhite__box1">
-				<span class="ah-managerwhite__span1">Помогите пожалуйста</span>
+			<div class="ah-managerentry__whitebox ah-managerwhite">
+				<div class="ah-managerwhite__box1">
+					<span class="ah-managerwhite__span1">Денег в хату, здоровья семье и близким</span>
+				</div>
+				<span class="ah-managerwhite__span">10:15</span>
 			</div>
-			<span class="ah-managerwhite__span">10:16</span>
-		</div>
-		<div class="ah-managerentry__graybox ah-managergray">
-			<div class="ah-managergray__box">
-				<span class="ah-managerentry-span">Всегда рада помочь :)</span>
-				<span class="ah-managerentry-span">Обращайтесь!</span>
+			<div class="ah-managerentry__whitebox ah-managerwhite">
+				<div class="ah-managerwhite__box1">
+					<span class="ah-managerwhite__span1">Помогите пожалуйста</span>
+				</div>
+				<span class="ah-managerwhite__span">10:16</span>
 			</div>
-			<span class="ah-managergray__span">20:21</span>
+			<div class="ah-managerentry__graybox ah-managergray">
+				<div class="ah-managergray__box">
+					<span class="ah-managerentry-span">Всегда рада помочь :)</span>
+					<span class="ah-managerentry-span">Обращайтесь!</span>
+				</div>
+				<span class="ah-managergray__span">20:21</span>
+			</div>
 		</div>
 	</div>
 	<div class="ah-manager__textbox ah-managertext">
