7381642b4710b1747f9ce16600f9a83eb2567452
iwebnal <iwebnal@gmail.com>
2017-05-20 18:24:27 +0300

    a

--- app/include/discounts.php
+++ app/include/discounts.php
@@ -66,6 +66,21 @@
 					</div>
 				</div>
 			</li>
+			<li class="ah-accord-list__item ah-accord">
+				<a href="#" class="ah-accord__link">Fuels, Oils and Canisters</a>
+				<div class="ah-accord__content">
+					<a href="#" class="ah-accord__close"></a>
+					<span class="ah-accord__title">Fuels, Oils and Canisters</span>
+					<div class="ah-accord__box1 ah-box1">
+						<span class="ah-box1__span">Период действия: <span>29.03.17 — 29.04.17</span></span>
+						<span class="ah-box1__span1">Код скидки: <span>0000000000089</span></span>
+					</div>
+					<div class="ah-accord__box2 ah-box2">
+						<span class="ah-box2__span">Значимость для конечного клиента:<span>Нет</span></span>
+						<span class="ah-box2__span1">Можно заказать B2B:<span>Да</span></span>
+					</div>
+				</div>
+			</li>
 		</ul>
 	</div>
 </div>
--- app/include/footer.php
+++ app/include/footer.php
@@ -2,8 +2,23 @@
     <div class="container">
     	<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<div class="">
-					
+				<div class="ah-footer__box1 ahfooter1">
+					<div class="ahfooter1-left">
+						<span class="ahfooter1-span ahfooter1span1">Email:<span>B2BTest@stihl.gb</span></span>
+					</div>
+					<div class="ahfooter1-right">
+						<span class="ahfooter1-span ahfooter1span2">Телефон:<span>8 (980) 380-90-80</span></span>
+					</div>
+				</div>
+				<div class="ah-footer__box2 ahfooter2">
+					<div class="ahfooter2-left">
+						<span class="ahfooter2-span">ANDREAS STIHL LTD</span>
+						<span class="ahfooter2-span">STIHL House</span>
+					</div>
+					<div class="ahfooter2-right">
+						<span class="ahfooter2-span">Stanhope Road</span>
+						<span class="ahfooter2-span">Camberley Surrey GU15 3YT</span>
+					</div>
 				</div>
 			</div>
 		</div>
