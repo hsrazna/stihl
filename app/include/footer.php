6a0b3d20f298d8a218a210ebbd2d4bc0581770e3
iwebnal <iwebnal@gmail.com>
2017-05-20 17:20:05 +0300

    a

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,1 +1,11 @@
-footer.php
+<section class="ah-footer">
+    <div class="container">
+    	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
