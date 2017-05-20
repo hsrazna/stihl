c0272352b7f9440b0fd2a7f6981fd08e51e4b6ca
hsrazna <anzar.terek@gmail.com>
2017-05-20 18:15:18 +0300

    tabs table

--- app/include/documents.php
+++ app/include/documents.php
@@ -12,9 +12,27 @@
 		    </ul>
 		    <div class="ah-tabs__tabsbox ah-tabsbox">
 		        <div id="tab-1" class="ah-tabsbox__item">
-		            <a href="#" class="ah-tabsbox__link ah-tablink">Договор</a>
+		        	<div class="az-table az-table_full-width">
+						<div class="az-table__top">
+							<div class="az-table__head">
+								<div class="az-table__row">
+									<div class="az-table__head-td az-table__td_center az-table__col1">Дата <a href="#" class="az-sort">сортировать</a></div>
+									<div class="az-table__head-td az-table__col2">Название</div>
+								</div>
+							</div>
+						</div>
+						<div class="az-table__bottom">
+							<div class="az-table__body">
+								<div class="az-table__row">
+									<div class="az-table__td az-table__td_center az-table__col1">19.03.17</div>
+									<div class="az-table__td az-table__col2"><a href="#" class="ah-tablink">Договор</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
 		        </div>
 		        <div id="tab-2" class="ah-tabsbox__item">
+		            
 		            Контент второго таба
 		        </div>
 		        <div id="tab-3" class="ah-tabsbox__item">
--- app/js/common2.js
+++ app/js/common2.js
@@ -11,17 +11,30 @@ $(function() {
 		autoReinitialise: true,
 		showArrows: true,
 	}).bind('jsp-scroll-x', function(event, destX){
-		$(".az-table__top").css({ left: $(".az-table__bottom .jspPane").css("left")});
+		$(this).siblings(".az-table__top").css({ left: $(this).find(".jspPane").css("left")});
 	});
+
+	// $(".az-table__bottom2").jScrollPane({
+	// 	autoReinitialise: true,
+	// 	showArrows: true,
+	// }).bind('jsp-scroll-x', function(event, destX){
+	// 	$(".az-table__top2").css({ left: $(".az-table__bottom2 .jspPane").css("left")});
+	// });
 
 	$(".az-scroll").jScrollPane({
 		autoReinitialise: true,
 		showArrows: true,
 	});
-	
+
 });
 
 function setTableBody()
 {
-    $(".az-table__bottom").height($(".az-table").height());
+    $(".az-table__bottom").each(function(){
+    	$(this).height($(this).parents(".az-table").height());
+    });
+
+    // $(".az-table__bottom2").each(function(){
+    // 	$(this).height($(this).parents(".az-table").height());
+    // });
 }
