bc7cb13b987acfdb840b743295038cd8e6d916fe
iwebnal <iwebnal@gmail.com>
2017-05-20 20:56:13 +0300

    block requisites

--- app/include/requisites.php
+++ app/include/requisites.php
@@ -1,1 +1,25 @@
-<div class="container">header</div>
+<div class="ah-requisites">
+	<div class="ah-discounts__topbox ah-topbox ah-requisites-topbox">
+		<span class="ah-topbox__spanleft">Мои реквизиты</span>
+		<a href="#" class="ah-topbox__spanright">Редактировать</a>
+	</div>
+	<div class="ah-requisites__content ah-reqcontent">
+		<div class="ah-reqcontent-wrapp1">
+			<div class="ah-reqcontent-wrapp">
+				<span class="ah-reqcontent-span">Название организвации:</span>
+				<span class="ah-reqcontent-span ah-reqspan">Общество с ограниченной ответственностью “Свобода”</span>
+				<span class="ah-reqcontent-span">ИНН:<span>0000000000089</span></span>
+				<span class="ah-reqcontent-span">КПП:<span>0000000000089</span></span>
+				<span class="ah-reqcontent-span">ОГРН:<span>0000000000089</span></span>
+				<span class="ah-reqcontent-span">ОКВД:<span>12520004678352923400</span></span>
+			</div>
+			<div class="ah-reqcontent-wrapp ah-reqwrapp1">
+				<span class="ah-reqcontent-span">Юридический и фактический адрес:</span>
+				<span class="ah-reqcontent-span ah-reqspan">г. Москва, ул. Краснознаменная, 125</span>
+				<span class="ah-reqcontent-span">Почтовый индекс:<span>349007</span></span>
+				<span class="ah-reqcontent-span">Банковские реквизиты:</span>
+				<span class="ah-reqcontent-span ah-reqspan">г. Москва, ул. Краснознаменная, 125</span>
+			</div>
+		</div>
+	</div>
+</div>
