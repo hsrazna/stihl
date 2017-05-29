db84a03ce81679f9704ce4208cb0482896fe0f15
hsrazna <anzar.terek@gmail.com>
2017-05-29 12:20:59 +0300

    breadcrumbs and print

---
+++ app/include/catalog.php
@@ -0,0 +1,15 @@
+<div class="az-catalog">
+	<div class="container">
+		<div class="az-catalog__head clearfix">
+			<div class="az-catalog__breadcrumbs">
+				<ul class="az-breadcrumbs">
+					<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+					<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">Каталог</a></li>
+				</ul>
+			</div>
+			<div class="az-catalog__print">
+				<a href="#" class="az-print">Распечатать</a>
+			</div>
+		</div>
+	</div>
+</div>
