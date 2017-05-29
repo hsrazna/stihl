2ef4405c375206434857df56d2514cc1e7387c52
hsrazna <anzar.terek@gmail.com>
2017-05-29 20:19:26 +0300

    kit head

---
+++ app/include/kit.php
@@ -0,0 +1,71 @@
+<section class="ah-cardproduct">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		    	<div class="az-catalog__head clearfix">
+					<div class="az-catalog__breadcrumbs ah-breadcrumbs">
+						<ul class="az-breadcrumbs">
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Каталог</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">STIHL Machines & Accessories</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Cordless Power System PRO</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Cordless Tools</a></li>
+							<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">MSA 160 C-BQ Chainsaw</a></li>
+						</ul>
+					</div>
+					<div class="az-catalog__print ah-print">
+						<a href="#" class="az-print">Распечатать</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-cardproduct-wrapp">
+					<div class="ah-cardproduct__tabs ah-cardtabs">
+						<div class="ah-documents__content">
+							<div id="tabs" class="ah-tabs">
+							    <ul class="ah-tabs__topline ah-tabsheader">
+							        <li class="ah-cardtabs__item"><a href="#tab-1" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink">Карточка товара</a></li>
+							        <li class="ah-cardtabs__item"><a href="#tab-2" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1 ah-tablink2">Предмет / Наборы</a></li>
+							        <li class="ah-cardtabs__item"><a href="#tab-3" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1">Аксессуары</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-4" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1">Запасные части</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-5" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1 ah-tablink2">Инструменты</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-6" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink2">Технические документы</a></li>
+							    </ul>
+							    <div class="ah-tabs__tabsbox ah-tabsbox ah-cardtabsbox">
+							        <div id="tab-1" class="ah-tabsbox__item ah-cardtab1-wrapp" style="display: none;">
+							        	
+							        </div>
+							        <div id="tab-2" class="ah-tabsbox__item" style="display: block;">
+							        	<div class="az-kit">
+							        		<div class="clearfix">
+								        		<div class="az-kit__head">
+										            <h2 class="az-kit__title">MSA 160 C-BQ Cordless Chainsaw</h2>
+										            <b class="az-kit__subtitle">Введите ваш логин и пароль для того,чтобы зайти на сайт или зайдите самостоятельно</b>
+									            </div>
+									            <div class="az-kit__excel">
+									            	<a href="#" class="az-excel">Скачать в Excel</a>
+									            </div>
+								            </div>
+							            </div>
+							        </div>
+							        <div id="tab-3" class="ah-tabsbox__item">
+							            333 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							        <div id="tab-4" class="ah-tabsbox__item">
+							        	444 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							        <div id="tab-5" class="ah-tabsbox__item">
+							            555 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							        <div id="tab-6" class="ah-tabsbox__item">
+							            666 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
