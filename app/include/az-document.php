14f940e70676297bc8a4fc0f6375b8e19de5179c
hsrazna <anzar.terek@gmail.com>
2017-05-31 16:12:02 +0300

    document

---
+++ app/include/az-document.php
@@ -0,0 +1,127 @@
+<section class="ah-cardproduct">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		    	<div class="az-catalog__head clearfix">
+					<div class="az-catalog__breadcrumbs ah-breadcrumbs">
+						<ul class="az-breadcrumbs">
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Каталог</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">STIHL Machines & Accessories</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Cordless Power System PRO</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Cordless Tools</a></li>
+							<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">MSA 160 C-BQ Chainsaw</a></li>
+						</ul>
+					</div>
+					<div class="az-catalog__print ah-print">
+						<a href="#" class="az-print">Распечатать</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-cardproduct-wrapp">
+					<div class="ah-cardproduct__tabs ah-cardtabs">
+						<div class="ah-documents__content">
+							<div id="tabs" class="ah-tabs">
+							    <ul class="ah-tabs__topline ah-tabsheader">
+							        <li class="ah-cardtabs__item"><a href="#tab-1" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink">Карточка товара</a></li>
+							        <li class="ah-cardtabs__item"><a href="#tab-2" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1 ah-tablink2">Предмет / Наборы</a></li>
+							        <li class="ah-cardtabs__item"><a href="#tab-3" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1">Аксессуары</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-4" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1">Запасные части</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-5" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1 ah-tablink2">Инструменты</a></li>
+									<li class="ah-cardtabs__item"><a href="#tab-6" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink2">Технические документы</a></li>
+							    </ul>
+							    <div class="ah-tabs__tabsbox ah-tabsbox ah-cardtabsbox">
+							        <div id="tab-6" class="ah-tabsbox__item ah-cardtab1-wrapp">
+							        	1
+							        </div>
+							        <div id="tab-2" class="ah-tabsbox__item">
+							        	2
+							        </div>
+							        <div id="tab-3" class="ah-tabsbox__item">
+							            3
+							        </div>
+							        <div id="tab-4" class="ah-tabsbox__item">
+							        	444 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							        <div id="tab-5" class="ah-tabsbox__item">
+							            555 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							        <div id="tab-1" class="ah-tabsbox__item">
+							            <div class="az-kit">
+							        		<div class="clearfix">
+								        		<div class="az-kit__head">
+										            <h2 class="az-kit__title">MSA 160 C-BQ Cordless Chainsaw</h2>
+										            <b class="az-kit__subtitle">Введите ваш логин и пароль для того,чтобы зайти на сайт или зайдите самостоятельно</b>
+									            </div>
+									            <div class="az-kit__excel">
+									            	<a href="#" class="az-excel">Скачать в Excel</a>
+									            </div>
+								            </div>
+								            <form action="/">
+									            <div class="az-document__table az-table">
+													<div class="az-table__top">
+														<div class="az-table__head">
+															<div class="az-table__row">
+																<div class="az-table__head-td az-document__col1">Название <a href="#" class="az-sort">сортировать</a><a href="#" class="az-table-filter">фильтр</a></div>
+																<div class="az-table__head-td az-document__col2">Описание <a href="#" class="az-table-filter">фильтр</a></div>
+															</div>
+														</div>
+													</div>
+													<div class="az-table__bottom">
+														<div class="az-table__body">
+															<div class="az-table__row">
+																<div class="az-table__td az-document__col1"><a href="#" class="ah-tablink">MSA 160 C-BQ. Описание и характеристики</a></div>
+																<div class="az-table__td az-document__col2">Описание и характеристики</div>
+															</div>
+															<div class="az-table__row">
+																<div class="az-table__td az-document__col1"><a href="#" class="ah-tablink">MSA 160 C-BQ. Правила эксплуатации</a></div>
+																<div class="az-table__td az-document__col2">Правила эксплуатации</div>
+															</div>
+														</div>
+													</div>
+									            </div>
+									            <div class="az-kit__bottom clearfix">
+									            	<div class="az-kit__bottom-right">
+									            		<div class="az-number">
+										            		<span class="az-number__text">Количество</span>
+										            		<div class="az-number__select">
+											            		<div class="ah-selectbox">
+																	<select class="ah-select1">
+																		<option>10</option>
+																		<option>30</option>
+																		<option>50</option>
+																	</select>
+																</div>
+															</div>
+														</div>
+									            	</div>
+									            	<div class="az-kit__bottom-left az-kit__bottom-left_document">
+									            		<!-- <input type="submit" value="Добавить в корзину" class="az-button js-button-on-off-out"> -->
+									            	</div>
+									            	<div class="az-kit__bottom-center az-kit__bottom-center_document">
+									            		<div class="az-pagination">
+									            			<div class="az-pagination__item"><a href="#" class="az-pagination__left2"> << </a></div>
+									            			<div class="az-pagination__item"><a href="#" class="az-pagination__left"> < </a></div>
+									            			<div class="az-pagination__item">
+																<span>Страница</span>
+																<input type="text" value="1">
+																<span>из 125</span>
+									            			</div>
+									            			<div class="az-pagination__item"><a href="#" class="az-pagination__right"> > </a></div>
+									            			<div class="az-pagination__item"><a href="#" class="az-pagination__right2"> >> </a></div>
+									            		</div>
+									            	</div>
+								            	</form>
+								            </div>
+							            </div>
+							        </div>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
