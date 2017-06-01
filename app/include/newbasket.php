001a239ccd8907005ee32a3b9d6ec703ce27fcf6
iwebnal <iwebnal@gmail.com>
2017-06-02 02:05:53 +0300

    newbasket start

---
+++ app/include/newbasket.php
@@ -0,0 +1,147 @@
+<section class="ah-cardproduct">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		    	<div class="az-catalog__head clearfix">
+					<div class="az-catalog__breadcrumbs ah-breadcrumbs">
+						<ul class="az-breadcrumbs">
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+							<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">Новая корзина</a></li>
+						</ul>
+					</div>
+					<div class="az-catalog__print ah-print">
+						<a href="#" class="az-print">Распечатать</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-cardproduct-wrapp">
+					<div class="ah-cardproduct__tabs ah-cardtabs">
+						<div class="ah-documents__content">
+							<div id="tabs" class="ah-tabs">
+							    <ul class="ah-tabs__topline ah-tabsheader">
+							        <li class="ah-cardtabs__item"><a href="#tab-1" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink">Новая корзина</a></li>
+							        <li class="ah-cardtabs__item"><a href="#tab-2" class="ah-tabsheader-link ah-cardtabs-link ah-cardlink1 ah-tablink2">Товары</a></li>
+							    </ul>
+							    <div class="ah-tabs__tabsbox ah-tabsbox ah-cardtabsbox">
+							        <div id="tab-1" class="ah-tabsbox__item ah-cardtab1-wrapp">
+										<div class="row">
+								        	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+									        	<span class="ah-cardleftbox-span ah-zapchastibox-span">Новая корзина</span>
+									        	<span class="ah-zapchasti-span">Введите ваш логин и пароль для того, чтобы зайти на сайт или зайдите самостоятельно</span>
+								        	</div>
+											<div class="clearfix">
+												<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+													<div class="ah-newbasket-wrapper">
+														<div class="ah-newbasket-leftwrap">
+															<div class="ah-newbasket-leftbox1">
+																<form class="ah-newbasket-form">
+																	<span class="ah-newbasket-formtitle">Основное</span>
+																	<input type="text" value="Новая козина 1">
+																	<input type="text" placeholder="Номер">
+																	<textarea placeholder="Комментарий"></textarea>
+																</form>
+															</div>
+															<div class="ah-newbasket-leftbox2">
+																<form class="ah-newbasket-form1">
+																	<span class="ah-newbasket-formtitle">Условия</span>
+																	<div class="ah-newbasket-select">
+																		<select class="ah-select1">
+																			<option>Условия заказа</option>
+																			<option>Условия заказа1</option>
+																			<option>Условия заказа2</option>
+																		</select>
+																	</div>
+																	<div class="ah-newbasket-select1">
+																		<select class="ah-select1">
+																			<option>Условия заказа</option>
+																			<option>Условия заказа1</option>
+																			<option>Условия заказа2</option>
+																		</select>
+																	</div>
+																</form>
+															</div>
+														</div>
+														<div class="ah-newbasket-rightwrap">
+															<div class="ah-newbasket-rightbox1">
+																<form class="ah-newbasket-form2">
+																	<span class="ah-newbasket-formtitle">Адрес доставки</span>
+																	<div class="ah-newbasket-select2">
+																		<select class="ah-select1">
+																			<option>Адрес доставки</option>
+																			<option>Адрес доставки1</option>
+																			<option>Адрес доставки2</option>
+																		</select>
+																	</div>
+																	<input type="text" placeholder="Имя 1">
+																	<input type="text" placeholder="Имя 2">
+																	<input type="text" placeholder="Контактный телефон">
+																	<input type="text" placeholder="Почтовый код">
+																</form>
+															</div>
+															<div class="ah-newbasket-rightbox2">
+																<form class="ah-newbasket-form3">
+																	<div class="ah-newbasket-select3">
+																		<select class="ah-select1">
+																			<option>Страна</option>
+																			<option>Страна1</option>
+																			<option>Страна2</option>
+																		</select>
+																	</div>
+																	<input type="text" placeholder="Штат">
+																	<input type="text" placeholder="Город">
+																	<input type="text" placeholder="Улица">
+																	<input type="text" placeholder="Дом">
+																</form>
+															</div>
+														</div>
+														<div class="ah-newbasket-bottomwrap">
+															<div class="ah-newbasket-bottomleft">
+																<div class="ah-newbasket-bottomleftitle">
+																	<span class="ah-newbasket-botleftitle">История</span>
+																</div>
+																<div class="ah-newbasket-bottomleft1">
+																	<span>Кем создано<span>New Russian User</span></span>
+																	<span>Дата создания<span>11.04.2017</span></span>
+																</div>
+																<div class="ah-newbasket-bottomleft2">
+																	<span>Последние изменения внёс<span>New Russian User</span></span>
+																	<span>Дата последних изменений<span>11.04.2017</span></span>
+																</div>
+															</div>
+															<div class="ah-newbasket-bottomright">
+																<div class="ah-newbasket-bottomleftitle">
+																	<span class="ah-newbasket-botleftitle">Итого</span>
+																</div>
+																<div class="ah-newbasket-bottomright1">
+																	<span>Итого цена<span>-</span></span>
+																	<span>Итого вес<span>-</span></span>
+																</div>
+																<div class="ah-newbasket-bottomright2">
+																	<span>Скидка<span>-</span></span>
+																	<span>Дата подтверждения<span>-</span></span>
+																</div>
+															</div>
+														</div>
+														<div class="ah-newbasket-linkswrap">
+															<a href="#">Оформить заказ</a>
+															<a href="#">Сохранить</a>
+															<a href="#">Отменить</a>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+							        </div>
+							        <div id="tab-2" class="ah-tabsbox__item ah-cardtab1-wrapp">
+							            222 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam magnam velit at aut eius, quod consequuntur possimus accusantium! Quo molestias nesciunt voluptates, blanditiis dolorem doloremque. Dolorum sequi asperiores soluta vero!
+							        </div>
+							    </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
