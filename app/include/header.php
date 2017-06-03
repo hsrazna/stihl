5813ab7aaa9a81ddd1c17d662cd1a8ef357f9cb2
hsrazna <anzar.terek@gmail.com>
2017-06-03 20:04:16 +0300

    basket popup

--- app/include/header.php
+++ app/include/header.php
@@ -41,7 +41,71 @@
 			<div class="az-header__control">
 				<ul class="az-control">
 					<li class="az-control__item">
-						<a href="#" class="az-control__manager">Менеджер</a>
+						<a href="#" class="az-control__manager js-click">Менеджер</a>
+						<div class="az-header-popup js-show">
+							<div class="az-header-popup__head ah-topbox">
+								<span class="ah-topbox__spanleft">Мои корзины</span>
+								<a href="#" class="ah-topbox__spanright">Все корзины</a>
+								<a href="#" class="az-header-popup__close">закрыть</a>
+							</div>
+							<div class="az-header-popup__table">
+								<div class="az-table az-table_popup">
+									<div class="az-table__top">
+										<div class="az-table__head">
+											<div class="az-table__row">
+												<div class="az-table__head-td az-header-popup__col">Название</div>
+												<div class="az-table__head-td az-header-popup__col">Основная сумма</div>
+												<div class="az-table__head-td az-header-popup__col">Расчетная сумма</div>
+											</div>
+										</div>
+									</div>
+									<div class="az-table__bottom">
+										<div class="az-table__body">
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">New basket 2017</a></div>
+												<div class="az-table__td az-header-popup__col">14.650</div>
+												<div class="az-table__td az-header-popup__col">-</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">Chainsaw’s basket</a></div>
+												<div class="az-table__td az-header-popup__col">6.100</div>
+												<div class="az-table__td az-header-popup__col">4.840</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">New basket 2017</a></div>
+												<div class="az-table__td az-header-popup__col">14.650</div>
+												<div class="az-table__td az-header-popup__col">3.200</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">Chainsaw’s basket</a></div>
+												<div class="az-table__td az-header-popup__col">6.100</div>
+												<div class="az-table__td az-header-popup__col">4.840</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">New basket 2017</a></div>
+												<div class="az-table__td az-header-popup__col">14.650</div>
+												<div class="az-table__td az-header-popup__col">56.800</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">Chainsaw’s basket</a></div>
+												<div class="az-table__td az-header-popup__col">6.100</div>
+												<div class="az-table__td az-header-popup__col">4.840</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">New basket 2017</a></div>
+												<div class="az-table__td az-header-popup__col">14.650</div>
+												<div class="az-table__td az-header-popup__col">3.200</div>
+											</div>
+											<div class="az-table__row">
+												<div class="az-table__td az-header-popup__col"><a href="#" class="az-orange-link">Chainsaw’s basket</a></div>
+												<div class="az-table__td az-header-popup__col">6.100</div>
+												<div class="az-table__td az-header-popup__col">4.840</div>
+											</div>
+										</div>
+									</div>
+						        </div>
+							</div>
+						</div>
 					</li>
 					<li class="az-control__item">
 						<a href="#" class="az-control__basket">Корзины</a>
