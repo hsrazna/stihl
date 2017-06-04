a12efc89b8cf8f991d9b71ec77ebf7cee74aa9ea
iwebnal <iwebnal@gmail.com>
2017-06-05 00:58:22 +0300

    a

---
+++ app/include/chat.php
@@ -0,0 +1,29 @@
+<section class="ah-cardproduct">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		    	<div class="az-catalog__head clearfix">
+					<div class="az-catalog__breadcrumbs ah-breadcrumbs">
+						<ul class="az-breadcrumbs">
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+							<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">Чат с менеджером</a></li>
+						</ul>
+					</div>
+					<div class="az-catalog__print ah-print">
+						<a href="#" class="az-print">Распечатать</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-infowrapp">
+	        		<div class="clearfix">
+		        		<div class="az-kit__head">
+				            <h2 class="az-kit__title">Информация</h2>
+				            <span>Введите ваш логин и пароль для того, чтобы зайти на сайт или зайдите самостоятельно</span>
+			            </div>
+		            </div>
+	            </div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ app/include/info.php
@@ -0,0 +1,189 @@
+<section class="ah-cardproduct">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+		    	<div class="az-catalog__head clearfix">
+					<div class="az-catalog__breadcrumbs ah-breadcrumbs">
+						<ul class="az-breadcrumbs">
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Главная</a></li>
+							<li class="az-breadcrumbs__item"><a href="#" class="az-breadcrumbs__link">Заказы</a></li>
+							<li class="az-breadcrumbs__item"><a class="az-breadcrumbs__link">Мои корзины</a></li>
+						</ul>
+					</div>
+					<div class="az-catalog__print ah-print">
+						<a href="#" class="az-print">Распечатать</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-infowrapp">
+		        	<div class="az-kit ah-kit">
+		        		<div class="clearfix">
+			        		<div class="az-kit__head">
+					            <h2 class="az-kit__title">Информация</h2>
+					            <b class="az-kit__subtitle">Введите ваш логин и пароль для того, чтобы зайти на сайт или зайдите самостоятельно</b>
+				            </div>
+				            <div class="az-kit__excel">
+				            	<a href="#" class="az-excel">Скачать в Excel</a>
+				            </div>
+			            </div>
+			            <div class="clearfix">
+							<div class="ah-info-topbox">
+								<div class="ah-info-titlebox">
+									<span class="ah-info-title">Внешние ссылки</span>
+								</div>
+								<div class="ah-info-contentbox">
+									<div class="ah-info-leftbox">
+										<div class="ah-info-contentitem">
+											<div class="ah-info-imgbox">
+												<img src="img/ah-info1.jpg" class="img-responsive" alt="img">
+											</div>
+											<div class="ah-info-cont">
+												<span class="ah-info-contspan">Shell test</span>
+												<span class="ah-info-contspan1">Writing training document</span>
+												<a href="#" class="ah-info-contspan2">http://ourshed/</a>
+											</div>
+										</div>
+										<div class="ah-info-contentitem">
+											<div class="ah-info-imgbox">
+												<img src="img/ah-info2.jpg" class="img-responsive" alt="img">
+											</div>
+											<div class="ah-info-cont">
+												<span class="ah-info-contspan">STIHL Timbersports</span>
+												<span class="ah-info-contspan1">Writing training document</span>
+												<a href="#" class="ah-info-contspan2">http://ourshed/</a>
+											</div>
+										</div>
+									</div>
+									<div class="ah-info-rightbox">
+										<div class="ah-info-contentitem">
+											<div class="ah-info-imgbox">
+												<img src="img/ah-info3.jpg" class="img-responsive" alt="img">
+											</div>
+											<div class="ah-info-cont">
+												<span class="ah-info-contspan">STIHL Tool Shed</span>
+												<span class="ah-info-contspan1">Writing training document</span>
+												<a href="#" class="ah-info-contspan2">http://ourshed/</a>
+											</div>
+										</div>
+										<div class="ah-info-contentitem">
+											<div class="ah-info-imgbox">
+												<img src="img/ah-info4.jpg" class="img-responsive" alt="img">
+											</div>
+											<div class="ah-info-cont">
+												<span class="ah-info-contspan">STIHL.co.uk</span>
+												<span class="ah-info-contspan1">Writing training document</span>
+												<a href="#" class="ah-info-contspan2">http://ourshed/</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+			            </div>
+			            <div class="clearfix">
+							<div class="ah-info-bottombox">
+								<div class="ah-info-titlebox ah-info-titlebox1">
+									<span class="ah-info-title">Полезные файлы</span>
+								</div>
+								<div class="ah-info-contentbox">
+									<div class="ah-info-leftbox">
+										<div class="ah-info-contentitem2">
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info11.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info12.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+										</div>
+										<div class="ah-info-contentitem2">
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info11.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info13.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="ah-info-rightbox1">
+										<div class="ah-info-contentitem2">
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info11.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info14.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+										</div>
+										<div class="ah-info-contentitem2">
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info11.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+											<div class="ah-info-contentitem1">
+												<div class="ah-info-imgbox1">
+													<img src="img/ah-info13.png" class="img-responsive" alt="img">
+												</div>
+												<div class="ah-info-cont1">
+													<span class="ah-info-contspan">Каталог</span>
+													<span class="ah-info-contspan1">Обучающее видео</span>
+													<a href="#" class="ah-info-contspan2">Скачать</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+			            </div>
+		            </div>
+	            </div>
+			</div>
+		</div>
+	</div>
+</section>
