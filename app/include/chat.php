710fc8c537d0702f6c0e1b15e93cc8da5003b64d
iwebnal <iwebnal@gmail.com>
2017-06-05 01:46:12 +0300

    a

--- app/include/chat.php
+++ app/include/chat.php
@@ -17,11 +17,89 @@
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 				<div class="ah-infowrapp">
 	        		<div class="clearfix">
-		        		<div class="az-kit__head">
-				            <h2 class="az-kit__title">Информация</h2>
-				            <span>Введите ваш логин и пароль для того, чтобы зайти на сайт или зайдите самостоятельно</span>
+		        		<div class="az-kit__head ah-chat-wrapp">
+				            <h2 class="az-kit__title">Чат с менеджером</h2>
+				            <span class="ah-chat-subtitle">Введите ваш логин и пароль для того, чтобы зайти на сайт или зайдите самостоятельно</span>
 			            </div>
 		            </div>
+		            <div class="clearfix">
+			            <div class="ah-chat-userbox">
+			            	<div class="ah-chat-ubox">
+			            		<div class="ah-chat-imgbox">
+			            			<img src="img/ah-chat.jpg" class="img-responsive" alt="img">
+			            		</div>
+			            		<div class="ah-chat-ucontent">
+			            			<span class="ah-chat-uspan">Менеджер Ирина</span>
+			            			<span class="ah-chat-uspan1">Телефон:<span>8 (980) 670-90-90</span></span>
+			            			<span class="ah-chat-uspan1">Email:<span>irinam@stihl.com</span></span>
+			            		</div>
+			            	</div>
+			            </div>
+		            </div>
+		            <div class="clearfix">
+						<div class="az-scroll ah-scrollheight">
+							<div class="ah-chat-wrapp1">
+								<div class="ah-manager__bottombox ah-managerentry">
+									<div class="ah-managerentry__titlebox">
+										<span class="ah-managerentry-title">Сегодня</span>
+									</div>
+									<div class="ah-managerentry__graybox ah-managergray">
+										<div class="ah-managergray__box ah-chat__box">
+											<span class="ah-managerentry-span">Здравствуйте!</span>
+											<span class="ah-managerentry-span">Чем могу вам помочь?</span>
+										</div>
+										<span class="ah-managergray__span">09:30</span>
+									</div>
+									<div class="ah-managerentry__whitebox ah-managerwhite">
+										<div class="ah-managerwhite__box1 ah-chat__box">
+											<span class="ah-managerwhite__span1">Денег в хату, здоровья семье и близким</span>
+										</div>
+										<span class="ah-managerwhite__span">10:15</span>
+									</div>
+									<div class="ah-managerentry__whitebox ah-managerwhite">
+										<div class="ah-managerwhite__box1 ah-chat__box">
+											<span class="ah-managerwhite__span1">Помогите пожалуйста</span>
+										</div>
+										<span class="ah-managerwhite__span">10:16</span>
+									</div>
+									<div class="ah-managerentry__graybox ah-managergray">
+										<div class="ah-managergray__box ah-chat__box">
+											<span class="ah-managerentry-span">Всегда рада помочь :)</span>
+											<span class="ah-managerentry-span">Обращайтесь!</span>
+										</div>
+										<span class="ah-managergray__span">20:21</span>
+									</div>
+									<div class="ah-managerentry__whitebox ah-managerwhite">
+										<div class="ah-managerwhite__box1 ah-chat__box">
+											<span class="ah-managerwhite__span1">Денег в хату, здоровья семье и близким</span>
+										</div>
+										<span class="ah-managerwhite__span">10:15</span>
+									</div>
+									<div class="ah-managerentry__whitebox ah-managerwhite">
+										<div class="ah-managerwhite__box1 ah-chat__box">
+											<span class="ah-managerwhite__span1">Помогите пожалуйста</span>
+										</div>
+										<span class="ah-managerwhite__span">10:16</span>
+									</div>
+									<div class="ah-managerentry__graybox ah-managergray">
+										<div class="ah-managergray__box ah-chat__box">
+											<span class="ah-managerentry-span">Всегда рада помочь :)</span>
+											<span class="ah-managerentry-span">Обращайтесь!</span>
+										</div>
+										<span class="ah-managergray__span">20:21</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="ah-manager__textbox ah-managertext">
+							<div class="ah-chat-wrapp1">
+								<form class="ah-managertext-form ah-chat-form">
+									<input type="text" value="Здравствуйте! У меня вопрос - подскажите пожалуйста как мне сделать так ">
+									<input type="submit" value="">
+								</form>
+							</div>
+						</div>
+					</div>
 	            </div>
 			</div>
 		</div>
